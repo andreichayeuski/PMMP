723498a2b8d9498d91e9c4b4b74017ef9ceea7df
Андрей Чаевский <andreichayeuski@gmail.com>
2019-12-16 23:32:37 +0300

    audio added

---
+++ PMMP_Lab13/Assets/Scripts/PuckScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PuckScript : MonoBehaviour
+{
+    public ScoreScript ScoreScriptInstance;
+    public static bool WasGoal { get; private set; }
+    private Rigidbody2D rb;
+    public AudioManager audioManager;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        WasGoal = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!WasGoal)
+        {
+            if (other.tag == "LeftPlayerGoal")
+            {
+                ScoreScriptInstance.Increment(ScoreScript.Score.RightPlayerScore);
+                WasGoal = true;
+                audioManager.PlayGoal();
+                StartCoroutine(ResetPuck());
+            }
+            else if (other.tag == "RightPlayerGoal")
+            {
+                ScoreScriptInstance.Increment(ScoreScript.Score.LeftPlayerScore);
+                WasGoal = true;
+                audioManager.PlayGoal();
+                StartCoroutine(ResetPuck());
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        audioManager.PlayPuckCollision();
+    }
+
+    private IEnumerator ResetPuck()
+    {
+        yield return new WaitForSecondsRealtime(0.3f);
+        WasGoal = false;
+        rb.velocity = rb.position = new Vector2(0, 0);
+    }
+}
---
+++ PMMP_Lab13/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public enum Score
+    {
+        LeftPlayerScore, RightPlayerScore
+    }
+
+    public Text LeftPlayerScoreTxt, RightPlayerScoreTxt;
+    private int leftPlayerScore, rightPlayerScore;
+
+    public void Increment(Score whichScore)
+    {
+        if (whichScore == Score.LeftPlayerScore)
+        {
+            LeftPlayerScoreTxt.text = (++leftPlayerScore).ToString();
+        }
+        else
+        {
+            RightPlayerScoreTxt.text = (++rightPlayerScore).ToString();
+        }
+    }
+}
